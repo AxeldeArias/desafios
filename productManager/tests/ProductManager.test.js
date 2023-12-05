943d7b4cde920e9987cc22d5c07a9ada01bcf093
Axel <axel@ip-192-168-100-41.us-west-1.compute.internal>
2023-12-05 15:42:39 -0300

    feat: change tests description

--- productManager/tests/ProductManager.test.js
+++ productManager/tests/ProductManager.test.js
@@ -11,7 +11,7 @@ beforeEach(async () => {
     .catch(() => {});
 });
 
-test("when a new Product Manager is created they should have an empty product list", async () => {
+test("init with empty product list", async () => {
   const newProductManager = new ProductManager({
     nombre: "Axel",
     path: FILE_PATH,
@@ -21,7 +21,81 @@ test("when a new Product Manager is created they should have an empty product li
   expect(productList).toEqual([]);
 });
 
-test("when a product without a mandatory properties is added, the product list should not have this one ", async () => {
+test("add new products", async () => {
+  const newProductManager = new ProductManager({
+    nombre: "Axel",
+    path: FILE_PATH,
+  });
+
+  await newProductManager.addProduct(sojaProduct);
+  await newProductManager.addProduct(atunProduct);
+
+  const productList = await newProductManager.getProducts();
+
+  expect(productList[0]).toEqual({ ...sojaProduct, id: 0 });
+  expect(productList[1]).toEqual({ ...atunProduct, id: 1 });
+  expect(productList.length).toBe(2);
+});
+
+test("update product", async () => {
+  const newDescription = "soja refinada";
+
+  const newProductManager = new ProductManager({
+    nombre: "Axel",
+    path: FILE_PATH,
+  });
+
+  const newProduct = await newProductManager.addProduct(sojaProduct);
+  expect(newProduct).toEqual({ ...sojaProduct, id: 0 });
+
+  await newProductManager.updateProduct(newProduct.id, {
+    description: newDescription,
+  });
+
+  const products = await newProductManager.getProducts();
+
+  expect(products[0]).toEqual({
+    ...sojaProduct,
+    description: newDescription,
+    id: 0,
+  });
+});
+
+test("delete product", async () => {
+  const newProductManager = new ProductManager({
+    nombre: "Axel",
+    path: FILE_PATH,
+  });
+
+  await newProductManager.addProduct(sojaProduct);
+  await newProductManager.deleteProduct(sojaProduct);
+
+  const products = await newProductManager.getProducts();
+
+  expect(products).not.toContainEqual({
+    ...sojaProduct,
+    id: 0,
+  });
+  expect(products.length).toBe(0);
+});
+
+test("throw product already exists error", async () => {
+  const newProductManager = new ProductManager({
+    nombre: "Axel",
+    path: FILE_PATH,
+  });
+
+  await newProductManager.addProduct(sojaProduct);
+  expect(newProductManager.addProduct(sojaProductDuplicated)).rejects.toThrow(
+    "El producto ya fue ingresado"
+  );
+  const products = await newProductManager.getProducts();
+
+  expect(products[0]).toEqual({ ...sojaProduct, id: 0 });
+  expect(products.length).toBe(1);
+});
+
+test("throw required attribute errors", async () => {
   const newProductManager = new ProductManager({
     nombre: "Axel",
     path: FILE_PATH,
@@ -59,78 +133,4 @@ test("when a product without a mandatory properties is added, the product list s
 
   const productList = await newProductManager.getProducts();
   expect(productList).toEqual([]);
-});
-
-test("when some products are added, the product list should contains them", async () => {
-  const newProductManager = new ProductManager({
-    nombre: "Axel",
-    path: FILE_PATH,
-  });
-
-  await newProductManager.addProduct(sojaProduct);
-  await newProductManager.addProduct(atunProduct);
-
-  const productList = await newProductManager.getProducts();
-
-  expect(productList[0]).toEqual({ ...sojaProduct, id: 0 });
-  expect(productList[1]).toEqual({ ...atunProduct, id: 1 });
-  expect(productList.length).toBe(2);
-});
-
-test("when an existing product there should be an error", async () => {
-  const newProductManager = new ProductManager({
-    nombre: "Axel",
-    path: FILE_PATH,
-  });
-
-  await newProductManager.addProduct(sojaProduct);
-  expect(newProductManager.addProduct(sojaProductDuplicated)).rejects.toThrow(
-    "El producto ya fue ingresado"
-  );
-  const products = await newProductManager.getProducts();
-
-  expect(products[0]).toEqual({ ...sojaProduct, id: 0 });
-  expect(products.length).toBe(1);
-});
-
-test("if a product is updated, the product list should have the changes", async () => {
-  const newDescription = "soja refinada";
-
-  const newProductManager = new ProductManager({
-    nombre: "Axel",
-    path: FILE_PATH,
-  });
-
-  const newProduct = await newProductManager.addProduct(sojaProduct);
-  expect(newProduct).toEqual({ ...sojaProduct, id: 0 });
-
-  await newProductManager.updateProduct(newProduct.id, {
-    description: newDescription,
-  });
-
-  const products = await newProductManager.getProducts();
-
-  expect(products[0]).toEqual({
-    ...sojaProduct,
-    description: newDescription,
-    id: 0,
-  });
-});
-
-test("if a product is deleted, the product list should have this one anymore", async () => {
-  const newProductManager = new ProductManager({
-    nombre: "Axel",
-    path: FILE_PATH,
-  });
-
-  await newProductManager.addProduct(sojaProduct);
-  await newProductManager.deleteProduct(sojaProduct);
-
-  const products = await newProductManager.getProducts();
-
-  expect(products).not.toContainEqual({
-    ...sojaProduct,
-    id: 0,
-  });
-  expect(products.length).toBe(0);
 });
