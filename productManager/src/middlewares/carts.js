f36d088444fd0f35ab94d7a81cd8ceb4ba5b4229
Axel <axel@Axels-MacBook-Pro-2.local>
2024-04-26 12:15:15 -0300

    fix: change atributte name

--- productManager/src/middlewares/carts.js
+++ productManager/src/middlewares/carts.js
@@ -1,5 +1,5 @@
 export const cartPermission = async (req, res, next) => {
-  if (req.user.cid !== req.params.cid) {
+  if (req.user.cartId !== req.params.cid) {
     return res
       .status(401)
       .json({ status: "error", error: "This cart is not yours" });
